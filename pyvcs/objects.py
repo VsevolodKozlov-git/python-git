4b314263a0d26671ee9eb27718979437f9c0a74b
VsevolodKozlov-git <71010414+VsevolodKozlov-git@users.noreply.github.com>
2021-04-24 11:52:03 +0300

    осталось доделать реализацию комманды

--- pyvcs/objects.py
+++ pyvcs/objects.py
@@ -9,6 +9,8 @@ import zlib
 from pyvcs.refs import update_ref
 from pyvcs.repo import repo_find
 
+class ObjectsNotFound(Exception):
+    pass
 
 def hash_object(data: bytes, fmt: str, write: bool = False) -> str:
     header = f'{fmt} {len(data)}\0'
@@ -24,21 +26,58 @@ def hash_object(data: bytes, fmt: str, write: bool = False) -> str:
             blob_file.write(zipped_store)
 
     return sha1
-    
-        
+
+
 def resolve_object(obj_name: str, gitdir: pathlib.Path) -> tp.List[str]:
-    # PUT YOUR CODE HERE
-    ...
-DDlo
+    '''
+    :return: Список хеш-кодов, имеющих начало == obj_name в дирректории gitdir
+    '''
+    if (len(obj_name) < 4) or (len(obj_name) > 40):
+        raise ObjectsNotFound(f'Not a valid object name {obj_name}')
+
+    found_shas = []
+    object_folder = gitdir / 'objects' / obj_name[:2]
+    obj_name_in_folder = obj_name[2:]
+    for obj in object_folder.iterdir():
+        if obj.stem[:len(obj_name_in_folder)] == obj_name_in_folder:
+            found_shas.append(obj_name[:2] + obj.stem)
+
+    if len(found_shas) == 0:
+        raise ObjectsNotFound(f'Not a valid object name {obj_name}')
+
+    return found_shas
+
 
 def find_object(obj_name: str, gitdir: pathlib.Path) -> str:
-    # PUT YOUR CODE HERE
-    ...
+    raise  Exception("Не реализовано!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
 
 def read_object(sha: str, gitdir: pathlib.Path) -> tp.Tuple[str, bytes]:
-    # PUT YOUR CODE HERE
-    ...
+    path = gitdir / 'objects' / sha[:2] / sha[2:]
+    with open(path, mode='rb') as file:
+        obj_data = zlib.decompress(file.read())
+
+    header, content = separate_obj_data(obj_data)
+    fmt, content_len = separate_header(header)
+    fmt = fmt.decode('ascii')
+
+    return fmt, content
+
+
+def separate_obj_data(obj_data: bytes) -> tp.Tuple[bytes, bytes]:
+    sep_index = obj_data.find(b'\x00')
+    header, content = separate_by_index(obj_data, sep_index)
+    return header, content
+
+
+def separate_header(header:bytes):
+    sep_index = header.find(b' ')
+    fmt, content_len = separate_by_index(header, sep_index)
+    return fmt, content_len
+
+
+def separate_by_index(obj_to_separate, sep_index:int):
+    return obj_to_separate[:sep_index], obj_to_separate[sep_index+1:]
 
 
 def read_tree(data: bytes) -> tp.List[tp.Tuple[int, str, str]]:
@@ -48,7 +87,13 @@ def read_tree(data: bytes) -> tp.List[tp.Tuple[int, str, str]]:
 
 def cat_file(obj_name: str, pretty: bool = True) -> None:
     # PUT YOUR CODE HERE
-    ...
+    gitdir = repo_find()
+    shas = resolve_object(obj_name, gitdir)
+    if len(shas) > 1:
+        raise Exception('specify longer name')
+    sha = shas[0]
+    object_content_bytes = read_object(sha, gitdir)[1]
+    object_content_decoded = object_content_bytes.decode('ascii')
 
 
 def find_tree_files(tree_sha: str, gitdir: pathlib.Path) -> tp.List[tp.Tuple[str, str]]:
