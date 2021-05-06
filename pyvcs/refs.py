6535c6448b67f5d044431ac05f029b2b785389b2
vsevolod <vsevolod026@gmail.com>
2021-05-06 18:25:55 +0300

    0.6.0 half of work done

--- pyvcs/refs.py
+++ pyvcs/refs.py
@@ -23,10 +23,29 @@ def resolve_head(gitdir: pathlib.Path) -> tp.Optional[str]:
 
 
 def is_detached(gitdir: pathlib.Path) -> bool:
-    # PUT YOUR CODE HERE
-    ...
+    '''
+
+    :param gitdir:
+    :return: Сбито ли значение HEAD с ветки?
+    '''
+    head_path = gitdir / 'HEAD'
+    with open(head_path, 'rb') as head_file:
+        head_data = head_file.read()
+    if head_data[:3] == b'ref':
+        return False
+    return True
 
 
 def get_ref(gitdir: pathlib.Path) -> str:
-    # PUT YOUR CODE HERE
-    ...
+    '''
+    :param gitdir:
+    :return: местоположение головы
+    '''
+    head_path = gitdir / 'HEAD'
+    with open(head_path, 'rb') as head_file:
+        head_data = head_file.read()
+    if head_data[:3] == b'ref':
+        head_path = head_data[5:].decode().rstrip()
+        return head_path
+    else:
+        return head_data.hex()
